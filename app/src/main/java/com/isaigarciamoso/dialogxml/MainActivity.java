7e9b467624584599374ba6f033b5d04d54768dac
isaigarciamoso <ing.isaimosso@gmail.com>
2017-03-21 23:15:10 -0600

    Initial commit

---
+++ app/src/main/java/com/isaigarciamoso/dialogxml/MainActivity.java
@@ -0,0 +1,49 @@
+package com.isaigarciamoso.dialogxml;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewDialog viewDialog = new ViewDialog();
+        viewDialog.showDialog(MainActivity.this,"Error de conexión al servidor");
+    }
+
+
+
+    public class ViewDialog {
+
+        public void showDialog(Activity activity, String msg){
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.dialog_layout);
+            
+            TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+            text.setText(msg);
+
+            Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+            dialogButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+
+        }
+    }
+
+}
